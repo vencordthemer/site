60249010b293bf9c18aa6a105d24c6df733b47d4
vencordthemer <vencordthemer@outlook.com>
2025-06-17 16:07:13 +0100

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,14 @@
 <script setup>
 import Site from './components/mainSite.vue'
+import Background from './components/Background.vue'
 </script>
 
 <template>
   <div class="app-container">
     <main>
+      <Background/>
       <Site />
+      
     </main>
   </div>
 </template>
